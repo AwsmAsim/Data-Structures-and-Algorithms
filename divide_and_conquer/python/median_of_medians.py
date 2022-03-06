6d8e634684dd985e724c2e75bb28f3aa9a5fe646
AwsmAsim <63421684+AwsmAsim@users.noreply.github.com>
2022-03-06 06:24:17 -0800

    Delete divide_and_conquer/python directory

--- divide_and_conquer/python/median_of_medians.py
+++
@@ -1,9 +0,0 @@
-given_arr = [13, 24, 4, 24, 3, 18, 19, 20, 23, 15, 22, 16, 1]
-
-# Arrange all the elements less than the pivot to the write
-# and more than the pivot to the right
-def pivotDivide():
-    pass
-
-def findMedian(given_arr):
-    pass
